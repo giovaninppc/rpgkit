22449acd7912a34ac1c5f68d6dfb16d51422aab6
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2018-06-13 10:09:41 -0300

    Created app

---
+++ RPG Kit/RPG Kit WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  RPG Kit WatchKit Extension
+//
+//  Created by Giovani Nascimento Pereira on 13/06/18.
+//  Copyright © 2018 Giovani Nascimento Pereira. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ RPG Kit/RPG Kit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RPG Kit
+//
+//  Created by Giovani Nascimento Pereira on 13/06/18.
+//  Copyright © 2018 Giovani Nascimento Pereira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

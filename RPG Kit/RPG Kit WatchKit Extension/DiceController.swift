d1484b5db3cca865df0806f870fbe046db85a933
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2018-06-13 10:34:11 -0300

    Created life counter and dice labels

--- RPG Kit/RPG Kit WatchKit Extension/DiceController.swift
+++ RPG Kit/RPG Kit WatchKit Extension/DiceController.swift
@@ -1,5 +1,5 @@
 //
-//  InterfaceController.swift
+//  DiceController.swift
 //  RPG Kit WatchKit Extension
 //
 //  Created by Giovani Nascimento Pereira on 13/06/18.
@@ -10,40 +10,38 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class DiceController: WKInterfaceController {
 
-    @IBOutlet var lifeLabel: WKInterfaceLabel!
+    @IBOutlet var slider: WKInterfaceSlider!
     
-    var life: Int = 20 {
-        didSet {
-            lifeLabel.setText("\(life)")
-        }
-    }
+    @IBOutlet var whichDice: WKInterfaceLabel!
+    
+    @IBOutlet var imageView: WKInterfaceImage!
+    
+    
+    let diceArray = [2, 4, 8, 10, 12, 20, 50, 100]
+    let diceName = ["Coin (D2)", "D4", "D8", "D10", "D12", "D20", "D50", "D100"]
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
     }
-    
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        slider.setNumberOfSteps(diceArray.count)
     }
-    
+
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
     
-    @IBAction func upButton() {
-        life += 1
+    @IBAction func sliderChangeValue(_ value: Float) {
+        let intValue = Int(value)
+        whichDice.setText("\(diceName[intValue])")
     }
     
-    @IBAction func downButton() {
-        life -= 1
-    }
-    
-    
-
 }
